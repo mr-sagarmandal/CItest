7cdef115d424ecd518b9c61ceecb83165c0a167c
Sagar Mandal <sagar187@hotmail.com>
2016-06-14 10:44:15 -0400

    Finding Location
    Python code to find approximate geo location.

---
+++ location.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from urllib2 import urlopen
+from contextlib import closing
+import json
+
+# Automatically geolocate the connecting IP
+url = 'http://freegeoip.net/json/'
+try:
+    with closing(urlopen(url)) as response:
+        location = json.loads(response.read())
+        print(location)
+        location_city = location['city']
+        location_state = location['region_name']
+        location_country = location['country_name']
+        location_zip = location['zipcode']
+except:
+    print("Location could not be determined automatically")
